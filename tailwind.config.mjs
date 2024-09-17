82f464890f32066edee6ceee288697c67ecc2665
pitarchmiguel <pitarchmiguel@gmail.com>
2024-09-17 22:16:01 +0200

    subidas imagenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,9 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'program-1': "url('/src/assets/program1.jpg')",
-				'program-2': "url('/src/assets/program2.jpg')",
-				'program-3': "url('/src/assets/program3.jpg')",
+				'program-1': "url('../../public/assets/program1.jpg')",
+				'program-2': "url('../../public/assets/program2.jpg')",
+				'program-3': "url('../../public/assets/program3.jpg')",
 			},
 			borderRadius: {
 				'lg': '1.5rem',
